b0b053f587575bfb78673628814651e6c7b9b5c1
Christoph <christoph.lindmaier@gmx.at>
2015-03-02 21:03:34 +0100

    force org.geotools to load from its repository, comment not working lines

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,14 +1,13 @@
 package controllers
 
-import at.fhtw.leiwi.wfsconnector._
 import play.api.mvc._
 
 object Application extends Controller {
 
   def index = Action {
-    val geoFeature = new GeoFeatureServiceImpl()
+//    val geoFeature = new GeoFeatureServiceImpl()
 
-    val result = geoFeature.getGeoFeature
+//    val result = geoFeature.getGeoFeature
 
     Ok(views.html.index("Your new application is ready."))
 
--- wfsconnector/build.sbt
+++ wfsconnector/build.sbt
@@ -2,6 +2,6 @@ name := "wfsconnector"
 
 resolvers += "Open Source Geospatial Foundation Repository" at "http://download.osgeo.org/webdav/geotools"
 
-libraryDependencies +=  "org.geotools" % "gt-wfs" % "13-RC1" exclude("javax.media", "jai_core")
+libraryDependencies +=  "org.geotools" % "gt-wfs" % "13-RC1" from "http://download.osgeo.org/webdav/geotools"
 
 libraryDependencies +=  "com.google.guava" % "guava" % "16.0.1"
